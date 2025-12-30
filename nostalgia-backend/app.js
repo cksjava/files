bc75d324b46a3f24839b47ed0b076f99219737b4
Chandrakant Singh <chandrakantsingh@protonmail.com>
2025-12-31 02:04:31 +0530

    disbled hsts

--- nostalgia-backend/app.js
+++ nostalgia-backend/app.js
@@ -9,7 +9,11 @@ const { apiRouter } = require("./src/routes");
 
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    hsts: false, // don’t ever tell browsers to force https
+  })
+);
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(morgan("dev"));
