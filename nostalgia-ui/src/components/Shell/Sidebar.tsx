ff09a55a68839ddda1a40e73ac520bc49ceb0a89
Chandrakant Singh <chandrakantsingh@protonmail.com>
2025-12-31 01:27:02 +0530

    Fixed errors related to unwanted imports

--- nostalgia-ui/src/components/Shell/Sidebar.tsx
+++ nostalgia-ui/src/components/Shell/Sidebar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWaveSquare, faGear, faLayerGroup, faMusic } from "@fortawesome/free-solid-svg-icons";
