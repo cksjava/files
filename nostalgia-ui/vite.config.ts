e1307c564bfb798b50a08b8bb960afcbae131675
Chandrakant Singh <chandrakantsingh@protonmail.com>
2025-12-31 01:46:57 +0530

    Fixed path for vite app

--- nostalgia-ui/vite.config.ts
+++ nostalgia-ui/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",   // ✅ important for serving from backend root
   plugins: [react(), tailwindcss()],
 })
